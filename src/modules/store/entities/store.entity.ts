125a4ea03a8986d5b34eb075beee6432d1fa6646
alex5 <angrymarshall@gmail.com>
2025-07-04 16:54:53 +0300

    feat() code

--- src/modules/store/entities/store.entity.ts
+++ src/modules/store/entities/store.entity.ts
@@ -1,1 +1,9 @@
-export class StoreEntity {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class StoreEntity {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  address: string;
+}

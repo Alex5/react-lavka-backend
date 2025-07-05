6c7a70b2c5d94b5e6c8d57f3aa69214865e256ee
alex5 <angrymarshall@gmail.com>
2025-07-05 16:22:51 +0300

    feat(mogrations) add new migration

---
+++ src/common/migrations/1751721219235-init-db.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InitDb1751721219235 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
